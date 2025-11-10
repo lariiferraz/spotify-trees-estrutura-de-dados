b5e7a08c552c3327d7070f88613b49774f8fe5a8
Larissa <lala.objetivo@gmail.com>
2025-11-10 14:31:08 -0300

    TEST: Teste das árvores AVL e RedBlack

---
+++ tests/test_redblack.py
@@ -0,0 +1,26 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# test_rbt_manual.py
+from trees import RedBlackTree
+
+# Criar a árvore Red-Black
+arvore = RedBlackTree()
+
+# Inserir alguns nós manualmente (chave = popularidade)
+arvore.inserir(50, {'track_name': 'Song A', 'artist_name': 'Artist 1', 'genre': 'Pop'})
+arvore.inserir(70, {'track_name': 'Song B', 'artist_name': 'Artist 2', 'genre': 'Rock'})
+arvore.inserir(30, {'track_name': 'Song C', 'artist_name': 'Artist 3', 'genre': 'Jazz'})
+arvore.inserir(60, {'track_name': 'Song D', 'artist_name': 'Artist 4', 'genre': 'Pop'})
+arvore.inserir(80, {'track_name': 'Song E', 'artist_name': 'Artist 5', 'genre': 'Hip-Hop'})
+
+# Buscar um nó
+resultado = arvore.buscar(70)
+print("\nBusca por popularidade 70:", resultado)
+
+# Altura da árvore
+print("Altura da árvore:", arvore.altura())
+
+# Comparações realizadas
+print("Comparações realizadas:", arvore.contador_comparacoes(), "\n")
